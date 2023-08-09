0b96a764049c6e8638decdaafebf11c60d1adbe4
wungthing <wungthingkashungnao123@gmail.com>
2023-08-09 14:49:50 +0530

    completed micro project 4

---
+++ app.js
@@ -0,0 +1,28 @@
+let buttons = document.querySelectorAll("button");
+let screenResult = document.querySelector(".screen");
+let calculation = new Array(); //array to store the operations and numbers
+let screenOutput; //varaible used to final reslult in string format
+
+function calculate(button) {
+  const value = button.textContent;
+  if (value === "RESET") {
+    calculation = [];
+    screenResult.textContent = "";
+  } else if (value === "DEL") {
+    calculation.pop();
+    screenResult.textContent = calculation.join("");
+  } else if (value === "=") {
+    let calcString = eval(screenOutput).toString(); //evaluating the screen output and converting the result to string
+    calculation = [...calcString]; //using spred operator to convert string to array
+
+    screenResult.textContent = eval(screenOutput);
+  } else {
+    calculation.push(value);
+    screenOutput = calculation.join("");
+    screenResult.textContent = screenOutput;
+  }
+}
+
+buttons.forEach((button) =>
+  button.addEventListener("click", () => calculate(button))
+);
